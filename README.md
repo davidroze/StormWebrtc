e4e86c8ebb47332fbf7f666a709d64fdb624660a
minche <avihay@walla.net.il>
2018-02-04 12:00:35 +0200

    test server

--- README.md
+++ README.md
@@ -15,6 +15,3 @@ StormWebrtcClientTest/Main.cpp
 
 Based on techniques described in this article
 http://www.stormbrewers.com/blog/webrtc-data-channels-without-signaling-aka-hacking-the-crap-out-of-webrtc/
-
-test commit fdsfds ddssdfsdfsdf
-trying sadsa
--- StormWebrtcTest/Main.cpp
+++ StormWebrtcTest/Main.cpp
@@ -1,4 +1,3 @@
-
 #include "StormWebrtc/StormWebrtc.h"
 #include "StormWebrtcServerAPI/StormWebrtcServer.h"
 
@@ -6,8 +5,10 @@ int main()
 {
   StormWebrtcStaticInit();
 
+  static const int kMaxConnections = 256;
+
   StormWebrtcServerSettings settings;
-  settings.m_MaxConnections = 256;
+  settings.m_MaxConnections = kMaxConnections;
   settings.m_Port = 61200;
   settings.m_KeyFile = "localhost.key";
   settings.m_CertFile = "localhost.crt";
@@ -18,6 +19,17 @@ int main()
 
   StormWebrtcEvent ev;
 
+  struct ClientData
+  {
+    bool m_HasClient = false;
+    bool m_GotURL = false;
+    StormWebrtcConnectionHandle m_ConnectionHandle;
+
+    // Some time of http request handle here
+  };
+
+  ClientData clients[kMaxConnections];
+
   while (true)
   {
     server->Update();
@@ -26,21 +38,36 @@ int main()
       switch (ev.m_Type)
       {
       case StormWebrtcEventType::kConnected:
-        printf("Connected\n");
+        clients[ev.m_ConnectionHandle.m_SlotId].m_HasClient = true;
+        clients[ev.m_ConnectionHandle.m_SlotId].m_GotURL = false;
+        clients[ev.m_ConnectionHandle.m_SlotId].m_ConnectionHandle =
+ev.m_ConnectionHandle;
         break;
       case StormWebrtcEventType::kDisconnected:
-        printf("Disconnected\n");
+        clients[ev.m_ConnectionHandle.m_SlotId].m_HasClient = false;
+
+        // Cancel the http request if it's still going here
+
         break;
       case StormWebrtcEventType::kData:
-        printf("Data\n");
-        server->SendPacket(ev.m_ConnectionHandle, "craps", 5, 0, true);
+
+        if(clients[ev.m_ConnectionHandle.m_SlotId].m_GotURL == false)
+        {
+          clients[ev.m_ConnectionHandle.m_SlotId].m_GotURL = true;
+
+          // Convert the data passed in to a zero delimited string
+          std::string url(ev.m_Buffer.get(), ev.m_Buffer.get() +
+ev.m_DataSize);
+
+          // Start the http request here using the url
+        }
         break;
       }
     }
+
+    // Check for http response completions here
   }
 
   StormWebrtcStaticCleanup();
   return 0;
 }
-
-
